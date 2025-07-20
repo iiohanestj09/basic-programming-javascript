10e9a904b3c8ee0eaa27d7488d672b4fda1722ea
iiohanestj09 <putra.dae2005@gmail.com>
2025-07-20 09:49:56 +0700

    Menambah materi for_while_doWhile menjadi 1 file

---
+++ c_struktur_kontrol/16_for_while_doWhile.js
@@ -0,0 +1,19 @@
+// For Loop
+for (let i = 0; i < 5; i++) {
+    console.log(`Perulangan For Ke-${i+1}`);
+}
+
+
+// While Loop
+let count = 3;
+while (count > 0) {
+    console.log(`Hitung Mundur: ${count}`);
+    count--;
+}
+
+// Do While
+let attempt = 0;
+do {
+    console.log("Menjalankan proses...");
+    attempt++;
+} while (attempt < 3);
