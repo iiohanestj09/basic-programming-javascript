2da63755b5988e4912871640ff5ec731e85ae44b
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-02 12:35:30 +0700

    Menambah materi properti_n_nilai

---
+++ e_object/35_properti_n_nilai.js
@@ -0,0 +1,41 @@
+/* 1. Properti adalah pasangan key: value
+key harus berupa string (secara implisit)
+value bisa berupa tipe data apa saja
+*/ 
+const pengguna = {
+    nama: "Putra",
+    umur: 20,
+    greet: function() {
+        return `Hallo, ini dengan ${this.nama}`
+    },
+
+    // Jika key memiliki spasi atau diawali angka, gunakan petik
+    "tanggal lahir": "9 Mei 2005",
+    "123eee": "abcd"
+};
+
+
+// 2. Mengakses Properti ada 2 cara
+    // A. Dot Notation
+console.log(pengguna.nama);
+console.log(pengguna.greet());
+
+    //B. Bracket Notation
+console.log(pengguna["nama"]);
+console.log(pengguna["greet"]());
+
+console.log(pengguna["tanggal lahir"]);      // Kelebihan bracket notation
+console.log(pengguna["123eee"]);
+
+
+// 3. Menghapus Properti
+delete pengguna.tahun;
+delete pengguna["tanggal lahir"];
+
+
+// 4. Key Dinamis
+const contohKey = "judul";
+const buku = {
+    [contohKey]: "Belajar Js"
+};
+console.log(buku.judul);
