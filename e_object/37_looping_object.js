32e346071a0e8c3fc635726a3532a85384e49690
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-02 17:58:36 +0700

    Menambah materi looping_object

---
+++ e_object/37_looping_object.js
@@ -0,0 +1,33 @@
+// 1. for in Loop
+const user = {
+    nama: "Putra",
+    umur: 20,
+    kota: "Malang"
+};
+
+for (let key1 in user) {
+    console.log(key1, user[key1]);
+}
+
+
+// 2. Object.keys()
+// Mengambil array berisi semua key milik object
+const keys = Object.keys(user);
+console.log(keys);
+
+for (let key2 of keys) {
+    console.log(key2, user[key2]);
+}
+
+
+// 3. Object.values()
+// mengambil array berisi semua value milik object
+const values = Object.values(user);
+console.log(values);
+
+
+// 4. Object.entries()
+// Mengembalikkan array of array: [[key: value]...]
+for (let [key, value] of Object.entries(user)) {
+    console.log(key, value)
+}
