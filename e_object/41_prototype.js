f27445de57417ac64ebb5b97c21c6ee448e896ea
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-02 19:55:13 +0700

    Menambah materi prototype

---
+++ e_object/41_prototype.js
@@ -0,0 +1,29 @@
+/* 1. __proto__
+- Adalah properti internal milik setiap objek di JS
+- Isinya adalah referensi ke objek prototype parent-nya
+- Tujuannya: kalauh sebuah objek tidak memiliki properti/method yang diminta, JS akan mencari ke atas melalui __proto__. Akan berhenti jika ketemu atau tidak ada sama sekali hingga akhir.
+*/
+
+const induk = { pekerjaan: "Programmer" };
+const anak = { nama: "Jonathan" };
+
+console.log(anak.pekerjaan);    // anak tidak punya properti pekerjaan
+anak.__proto__ = induk;         // Menghubungkan (chain)
+console.log(anak.pekerjaan);    
+// Karena propertinya tidak ada di aslinya, JS menelusuri ke Prototype Parent
+
+
+// 2. Object.create(protoObj)
+// Membuat object baru dan menentukan siapa "induk"-nya.
+// Object baru itu akan mewarisi properti dan method dari yang ditentukan sebelumnya
+
+const user = {
+    nama: "Putra",
+    greet() {
+        console.log(`Haloo Guniaa, dari ${this.nama}`);
+    }
+};
+
+const penyurat = Object.create(user);   // object baru, anak dari 'user'
+penyurat.tujuan = "Drian";
+penyurat.greet();
