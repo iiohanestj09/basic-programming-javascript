1c0a3afbde9a5b98ece832376695f18fe25c1301
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-02 12:53:18 +0700

    Menambah materi nested_object

---
+++ e_object/36_nested_object.js
@@ -0,0 +1,31 @@
+const user = {
+    nama: "Putra",
+    profile: {
+        umur: 20,
+        alamat: {
+            jalan: "Jl. Merdeka",
+            kota: "Malang"
+        },
+        kontak: {
+            email: "putra@gmail.com",
+            "no hp": "08xxxxxxxxx" 
+        }
+    },
+    tambahan: {
+        "gol darah": "z"
+    }
+}
+
+// 1. Cara akses
+console.log(user.profile.kontak["no hp"]);
+// console.log(user.profile.kontak.ayam);   akan error dan menghentikan program
+
+
+// 2. Optional Chaining
+// Membuat akses lebih aman di data yang bersifat dinamis atau belum pasti
+console.log(user?.profile?.alamat?.jalan.apalah); // akan undefined
+
+
+// 3. Nullish Coalescing, beri nilai default
+console.log(user?.tambahan?.["gol darah"]);
+console.log(user?.tambahan?.apalah ?? "Kosong Broo!");
