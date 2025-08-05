6eb723a868659ecd570b1239d435ec4e5df44ae8
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-05 21:02:58 +0700

    Update materi method_penting

--- f_array/43_method_penting.js
+++ f_array/43_method_penting.js
@@ -16,21 +16,44 @@ console.log(contoh);
 - .map()    --> melakukan transformasi pada setiap elemen dan mengembalikkan array baru. (Tidak mengubah array asli)
 - .filter() --> Mengambil elemen yang memenuhi kondisi tertentu
 - .reduce() --> Menggabungkan semua nilai menjadi satu nilai akhir (akumulasi)
+- .splice(x, y) --> Menghapus elemen sesuai index juga banyaknya elemen ke kanan.
+                --> Menghapus elemen dari index x ke kanan sebanyak y 
+                --> Method ini mempengaruhi array asli.
+- .splice(x, y, z)  --> Menghapus dari index x sebanyak y
+                    --> Menambah value z di index x
 */ 
 
+
+// .map()
 const angka = [2, 4, 6];
 const kuadrat = angka.map(n => n ** 2);
 console.log(angka);
 console.log(kuadrat);
 
-
+// .filter()
 const nilai = [80, 65, 90, 57];
 const kkm = nilai.filter(n => n >= 70);
 console.log(nilai); 
 console.log(kkm);
 
-
+// .reduce()
 const coba = [1, 2, 3, 4];
 const kurang = coba.reduce((result, num) => result - num, 10);
 console.log(coba);
 console.log(kurang);
+
+// .splice(x, y)    -> Menghapus
+const coba2 = [3, 5, 7, 9];
+coba2.splice(1, 2);
+console.log(coba2);
+coba2.splice(0, 1);
+console.log(coba2);
+
+// .splice(x, y, z)
+const coba3 = ["p", 99, true, "ayam", 100];
+coba3.splice(1, 0, "utra");
+console.log(coba3);
+coba3.splice(4, 1, "Geprek");
+console.log(coba3);
+coba3.splice(9, 0, "Woilah");
+console.log(coba3);    // Tidak ada empty slot jika akses diluar batas
