8f24e3df44df731871bf3c9a2f80f724c652e5c3
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-05 21:36:47 +0700

    Update materi konversi_n_manipulasi

--- f_array/44_konversi_n_manipulasi.js
+++ f_array/44_konversi_n_manipulasi.js
@@ -1,12 +1,10 @@
 // 1. Konversi dari String ke Array -> .split()
-// Memecah string menjadi array berdasarkan pemisah tertentu
 const teks1 = "Yohanes,Putra,P.,Muwa,Dae";
 const result1 = teks1.split(",");
 console.log(result1);
 
 
 // 2. Konversi dari Array ke String -> .join()
-// Menggabungkan semua elemen array menjadi satu string dengan pemisah tertentu
 const teks2A = ["Apel", "Mangga", "Anggur"];
 const result2A = teks2A.join(" | ");
 console.log(result2A);
@@ -16,7 +14,7 @@ const result2B = teks2B.join("");
 console.log(result2B);
 
 
-// 3. Mengurutkan isi Array -> .sort()
+// 3. Mengurutkan isi Array -> .sort() [ascending, terkecil ke terbesar]  {Mempengaruhi array asli}
 const angka1 = [1, 5, 3, 10];
 console.log(angka1.sort());     // Method ini defaulnya untuk string comparasion
 
@@ -28,3 +26,14 @@ angka1.sort((a, b) => a - b);
 //              Jika hasilnya positif   -> b dulu baru a    (b, a)
 console.log(angka1);
 
+const teks3 = ["ayam", "anggur", "kambing", "jerapah", "zebra", true, 55, undefined];   // "5" = 53, "a" = 97  
+console.log(teks3.sort())
+
+// 4. Membalik isi array -> .reverse()  {Mempengaruhi array asli}
+const teks4 = ["domba", "sapi", "harimau", "tikus"];
+console.log(teks4.reverse());
+
+const angka2 = [5, 2, 8, 0, 7, 4, 17, 13, 19, 14, 13];
+angka2.sort((a, b) => a - b);
+angka2.reverse();
+console.log(angka2);
