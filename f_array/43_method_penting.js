5e91fd7f62dc79b24d3e7c0eb0c0e86ba2f78e41
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-05 22:19:57 +0700

    Update materi method_penting

--- f_array/43_method_penting.js
+++ f_array/43_method_penting.js
@@ -12,44 +12,39 @@ console.log(contoh);
 contoh.pop();
 console.log(contoh);
 
-/* 2. Method Transformasi dan Iterasi
-- .map()    --> melakukan transformasi pada setiap elemen dan mengembalikkan array baru. (Tidak mengubah array asli)
-- .filter() --> Mengambil elemen yang memenuhi kondisi tertentu
-- .reduce() --> Menggabungkan semua nilai menjadi satu nilai akhir (akumulasi)
-- .splice(x, y) --> Menghapus elemen sesuai index juga banyaknya elemen ke kanan.
-                --> Menghapus elemen dari index x ke kanan sebanyak y 
-                --> Method ini mempengaruhi array asli.
-- .splice(x, y, z)  --> Menghapus dari index x sebanyak y
-                    --> Menambah value z di index x
-*/ 
-
-
-// .map()
+// 2. Method Transformasi dan Iterasi
+// .map()  -> melakukan transformasi pada setiap elemen dan mengembalikkan array baru. (Tidak mengubah array asli)
 const angka = [2, 4, 6];
 const kuadrat = angka.map(n => n ** 2);
 console.log(angka);
 console.log(kuadrat);
 
-// .filter()
+
+// .filter() -> Mengambil elemen yang memenuhi kondisi tertentu
 const nilai = [80, 65, 90, 57];
 const kkm = nilai.filter(n => n >= 70);
 console.log(nilai); 
 console.log(kkm);
 
-// .reduce()
+
+// .reduce() -> Menggabungkan semua nilai menjadi satu nilai akhir (akumulasi)
 const coba = [1, 2, 3, 4];
 const kurang = coba.reduce((result, num) => result - num, 10);
 console.log(coba);
 console.log(kurang);
 
-// .splice(x, y)    -> Menghapus
+
+// .splice(x, y) -> Menghapus elemen sesuai index juga banyaknya elemen ke kanan.
+//               -> Menghapus elemen dari index x ke kanan sebanyak y 
+//               -> Method ini mempengaruhi array asli.
 const coba2 = [3, 5, 7, 9];
 coba2.splice(1, 2);
 console.log(coba2);
 coba2.splice(0, 1);
 console.log(coba2);
 
-// .splice(x, y, z)
+// .splice(x, y, z)  -> Menghapus dari index x sebanyak y
+//                   -> Menambah value z di index x
 const coba3 = ["p", 99, true, "ayam", 100];
 coba3.splice(1, 0, "utra");
 console.log(coba3);
@@ -57,3 +52,19 @@ coba3.splice(4, 1, "Geprek");
 console.log(coba3);
 coba3.splice(9, 0, "Woilah");
 console.log(coba3);    // Tidak ada empty slot jika akses diluar batas
+
+
+// .indexof()  -> Menemukan index sebuah elemen pertama dari kanan array juga sebuah substring (5_operasi_string)
+const mixed = [10, undefined ,"ayam", 20, true, "ayam", 300, "ayam", "ayam", false, null];
+console.log(mixed.indexOf("ayam"));
+
+// Solusi Pencarian semua kemunculan
+const posisi = [];
+let index = mixed.indexOf("ayam");
+
+while (index !== -1) {
+    posisi.push(index);
+    index = mixed.indexOf("ayam", index + 1);   // Cari mulai dari setelah posisi sebelumnya
+                                                // Hingga di index 8, saat dicari lagi akan return -1 dan loop berhenti
+}
+console.log(posisi);
