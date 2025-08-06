bff7f226e66f3ede413c6a3e948de8d804d69818
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-06 17:36:25 +0700

    Menambah materi iterasi_n_konversi

---
+++ g_set/48_iterasi_n_konversi.js
@@ -0,0 +1,28 @@
+// 1. Iterasi Set
+// Untuk akses elemen Set, gunakan looping untuk setiap elemen
+
+  // A. for ... of
+  const buah = new Set(["Apel", "Mangga", "Anggur", "Apel"]);
+
+  for (const item of buah) {
+    console.log(item);
+  }
+
+  // B. .forEach()
+  buah.forEach((item) => console.log(item));
+
+// 2. Konversi Array <-> Set
+
+  // A. Array -> Set  (Harus duplikat)
+  const data = [1, 2, 2, 3];
+  const unik = new Set(data);
+  console.log(unik);
+
+  // B. Set -> Array  (Pakai Array.from())
+  const setBuah = new Set(["apel", "mangga"]);
+  const arrBuah = Array.from(setBuah);
+
+  //C. Array -> Set -> Array  (Biasanya untuk hapus duplikat)
+  const arrContoh = [1, 2, 3, 4, 3, 2, 5, 4, 6, 3];
+  const arrHasil = [...new Set(arrContoh)];
+  console.log(arrHasil);
