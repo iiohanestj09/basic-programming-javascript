bb0baf4242cc54f812cada9392b44e4624ac04e7
iiohanestj09 <putra.dae2005@gmail.com>
2025-08-06 17:08:51 +0700

    Update urutan tipe_data_complex

--- a_awal/4_tipe_data_complex.js
+++ a_awal/4_tipe_data_complex.js
@@ -4,15 +4,15 @@
 ----------------------------------------------------------------------------------
 | Tipe Data | Contoh               | Penjelasan Singkat                          |
 |-----------|----------------------|---------------------------------------------|
+| Function  | function() {}        | Fungsi yang bisa dipanggil                  |
 | Object    | { nama: "Putra" }    | Struktur data key-value                     |
 | Array     | [1, 2, 3]            | Kumpulan data dalam urutan indeks           |
-| Function  | function() {}        | Fungsi yang bisa dipanggil                  |
-| Date      | new Date()           | Representasi waktu dan tanggal              |
-| RegExp    | /abc/                | Pola pencocokan string                      |
-| Map       | new Map()            | Koleksi pasangan key-value unik             |
 | Set       | new Set()            | Koleksi nilai unik                          |
+| Map       | new Map()            | Koleksi pasangan key-value unik             |
 | WeakMap   | new WeakMap()        | Mirip Map, tapi key harus berupa objek      |
 | WeakSet   | new WeakSet()        | Mirip Set, tapi hanya menyimpan objek       |
+| Date      | new Date()           | Representasi waktu dan tanggal              |
+| RegExp    | /abc/                | Pola pencocokan string                      |
 | Error     | new Error("Oops")    | Representasi kesalahan                      |
 ----------------------------------------------------------------------------------
 */
